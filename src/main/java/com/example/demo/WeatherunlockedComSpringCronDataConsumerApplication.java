9652d76762dffda7c38c7b11c4216d031fc05e23
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-06-28 10:09:44 -0400

    enabled retry and hystrix support

--- src/main/java/com/example/demo/WeatherunlockedComSpringCronDataConsumerApplication.java
+++ src/main/java/com/example/demo/WeatherunlockedComSpringCronDataConsumerApplication.java
@@ -3,12 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.retry.annotation.EnableRetry;
 
 
 @EntityScan(basePackageClasses = { WeatherunlockedComSpringCronDataConsumerApplication.class, Jsr310JpaConverters.class })
 @EnableFeignClients
+@EnableRetry
+@EnableCircuitBreaker
 @SpringBootApplication
 public class WeatherunlockedComSpringCronDataConsumerApplication {
 
