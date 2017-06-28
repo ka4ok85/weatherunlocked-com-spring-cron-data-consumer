2efa2ffe024f39f7c49e435078fcb15692e07867
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-06-28 10:12:53 -0400

    switched to hystrixService

--- src/main/java/com/example/demo/service/WeatherService.java
+++ src/main/java/com/example/demo/service/WeatherService.java
@@ -12,45 +12,43 @@ import com.example.demo.dto.Call;
 import com.example.demo.entity.WeatherRecord;
 import com.example.demo.repository.WeatherRecordRepository;
 
-import feign.FeignException;
-
 @Service
 public class WeatherService {
 
 	private static final Logger log = LoggerFactory.getLogger(WeatherService.class);
-	
-    @Value("${service.name}")
-    private String serviceName;
+
+	@Value("${service.name}")
+	private String serviceName;
 
 	@Value("${weatherunlocked.appId}")
 	private String appId;
-	
+
 	@Value("${weatherunlocked.appKey}")
 	private String appKey;
-	
+
 	@Autowired
-    private WeatherRecordResource weatherRecordResource;
+	private HystrixWeatherService hystrixWeatherService;
 
 	@Autowired
 	private WeatherRecordRepository weatherRecordRepository;
 
 	public void run(Call call) {
-
 		float lat = call.getLatitude();
 		float lon = call.getLongitude();
 		LocalDateTime dateTime = call.getDatetime();
-    	
-		try {
-			// call Weather Service
-	    	WeatherRecord weatherRecord = weatherRecordResource.currentWeather(lat, lon, appId, appKey);
-	    	weatherRecord.setIncidentNumber(call.getIncidentNumber());
-	    	weatherRecord.setDatetime(dateTime);
-	    	weatherRecord.setZip(call.getZip());
-			weatherRecordRepository.save(weatherRecord);
 
-			log.info("Service: {}. Incident: {}. Fetched weatherRecord Object {}", serviceName, call.getIncidentNumber(), weatherRecord);
-	    } catch (FeignException e) {
-	    	log.error("Service: {}. Can not fetch data from: {}", serviceName, e.getMessage());
+		WeatherRecord weatherRecord = hystrixWeatherService.getWeather(lat, lon, appId, appKey);
+		if (weatherRecord == null) {
+			log.error("Service: {}. Incident: {}. Can not fetch Weather data!", serviceName, call.getIncidentNumber());
+			weatherRecord = new WeatherRecord();
 		}
+
+		weatherRecord.setIncidentNumber(call.getIncidentNumber());
+		weatherRecord.setDatetime(dateTime);
+		weatherRecord.setZip(call.getZip());
+
+		log.info("Service: {}. Incident: {}. Fetched weatherRecord Object {}", serviceName, call.getIncidentNumber(), weatherRecord);
+
+		weatherRecordRepository.save(weatherRecord);
 	}
 }
