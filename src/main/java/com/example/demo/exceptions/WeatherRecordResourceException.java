4fccddf4cc3117794e78cebfe09800d820c26bbd
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-06-28 10:10:23 -0400

    added new exception

---
+++ src/main/java/com/example/demo/exceptions/WeatherRecordResourceException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exceptions;
+
+public class WeatherRecordResourceException extends RuntimeException {
+
+	private static final long serialVersionUID = 4901501630076372631L;
+
+}
